87dec07c9916c030d70c850c9c5c49d059e764c4
timofeya123 <timofeya123@inbox.ru>
2021-02-17 21:38:34 +0600

    TryLock

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,44 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException {
+    //Разбираем TryLock
+    static Lock lock = new ReentrantLock();
 
+    //Разбираем TryLock
+    static class Thread1 extends Thread{
+        @Override
+        public void run(){ //
+            lock.lock();
+            System.out.println(getName()+" старт");
+            try {
+                sleep(5);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(getName()+" остановка работы");
+            lock.unlock();
+            System.out.println(getName()+" unlock");
+        }
+    }
+
+    //Разбираем TryLock
+    static class Thread2 extends Thread{
+        @Override
+        public void run(){ //
+            System.out.println(getName()+" старт");
+            while (true){
+                if (lock.tryLock()){
+                    System.out.println(getName()+" работает");
+                    break;
+                }else {
+                    System.out.println(getName()+" ждёт");
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        // Разбираем ReentrantLock 2
 	    Resource resource = new Resource(); // создаем ресурс
 	    resource.i = 5;
         resource.j = 3;
@@ -27,8 +63,13 @@ public class Main {
 
         System.out.println("i - "+resource.i); // вывод
         System.out.println("j - "+resource.j);
+
+        //Разбираем TryLock
+        new Thread1().start();
+        new Thread2().start();
     }
 
+    // Разбираем ReentrantLock 2
     static class MyThread extends Thread{ // сам поток
         Resource resource;
         @Override
@@ -39,6 +80,7 @@ public class Main {
     }
 }
 
+// Разбираем ReentrantLock 2
 class Resource{
     int i;
     int j;
